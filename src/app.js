52e8d325c909f11d6a89300fb28f296d890bd64e
Bytter <67333597+Byttter@users.noreply.github.com>
2022-09-27 20:30:39 -0300

    A rota do dashboard estava incorreta

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.set("view engine", "ejs")
 app.set("views", "src/frontend/views")
 app.use(express.static(__dirname + "/frontend/public/"))
 
-app.use("/admin/dashboard", get.emotes)
+app.use("/admin/dashboard", get.dashboard)
 app.use("/users/login/", get.login)
 app.use("/halloffame", get.halloffame)
 app.use("/clipes", get.clipes)
