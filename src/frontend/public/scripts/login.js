249913ca9539b320cb497373198f9d7ec1de24bf
Bytter <67333597+Bytters@users.noreply.github.com>
2022-11-16 15:10:03 -0300

    tirando o scope da url de acesso

--- src/frontend/public/scripts/login.js
+++ src/frontend/public/scripts/login.js
@@ -1,6 +1,6 @@
 const element = document.getElementById("login")
 const acess =
-    "https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=m2svdyi6a64r0b20cbmhte48x2pn6a&redirect_uri=https://tomfoolery.herokuapp.com/users/login&scope=user_read"
+    "https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=m2svdyi6a64r0b20cbmhte48x2pn6a&redirect_uri=https://tomfoolery.herokuapp.com/users/login&scope="
 
 element.addEventListener("click", () => {
     element.target = "_blank"
